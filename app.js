fb7985fe4c260182a87adf61352de44e1d6f2472
Waqarghori <waqar.iqbal789@gmail.com>
2025-07-03 19:17:48 +0500

    upd

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ fetch('https://dummyjson.com/products')
         getDiv.innerHTML += `<div class="card" style="width: 18rem;">
   <img src="${abc.images[0]}" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <h5 class="card-title">${abc.title}</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
