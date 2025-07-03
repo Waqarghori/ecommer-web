2fe709a7e7ebcea4282c7641e76d9cca9f6d37f4
Waqarghori <waqar.iqbal789@gmail.com>
2025-07-03 14:00:42 +0500

    upd

---
+++ app.js
@@ -0,0 +1,16 @@
+let getDiv = document.querySelector('.main')
+fetch('https://dummyjson.com/products')
+.then(res => res.json())
+.then((data)=>{
+    console.log(data.products);
+    data.products.map((abc)=>{
+        getDiv.innerHTML += `<div class="card" style="width: 18rem;">
+  <img src="${abc.images[0]}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`
+    })
+});
